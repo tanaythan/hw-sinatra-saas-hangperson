a69753b502a7aa279cd94af5b73e6055f363e47d
Tanay Nathan <tnathan@berkeley.edu>
2017-09-05 16:06:11 -0700

    Prevents user from winning

--- app.rb
+++ app.rb
@@ -63,12 +63,14 @@ class HangpersonApp < Sinatra::Base
   end
 
   get '/win' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+      redirect '/show'
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
     erb :lose # You may change/remove this line
   end
 
